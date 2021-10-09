2148892092647ed4b82496e434ecae294675d628
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2021-10-09 22:52:58 +0500

    Handling HTTP get request

--- Express/index.js
+++ Express/index.js
@@ -1,13 +1,29 @@
 const express= require("express")
 const app =express()
 
+const courses=[
+    {id:1,name:'courese1'},
+    {id:2,name:'courese2'},
+    {id:2,name:'courese3'}
+]
 app.get("/",(req,res)=>{
     res.send("Hello world")
 })
 
-app.get("/api/about",(req,res)=>{
-    res.send([1,2,3,4,5,6])
+app.get("/api/courses",(req,res)=>{
+   // res.send([1,2,3,4,5,6])
+   res.send(courses)
 })
-PORT=5000
+
+
+app.get("/api/courses/:id",(req,res)=>{
+   // res.send(req.params.id)
+  //res.send(req.params)
+  //res.send(req.query)
+  const cours=courses.find(c=> c.id===parseInt(req.params.id))
+  if(!cours) res.status(404).send("The course for the given id was not found") //404 object not found
+   res.send(cours)
+})
+
 const port =process.env.PORT || 3002
 app.listen(port,()=>console.log(`listing to port  ${port}`))
