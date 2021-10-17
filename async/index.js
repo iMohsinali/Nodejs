857f6ed7ef6d96185c8c64f4ba0b1431f4cc20f1
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2021-10-17 12:04:00 +0500

    promises

--- async/index.js
+++ async/index.js
@@ -1,27 +1,53 @@
 console.log("before")
-getUser(1,(user)=>{
-            githubrepos(user.name,(repo)=>{
-                console.log("repo",repo)
-            })
-})
+// getUser(1,(user)=>{
+//             githubrepos(user.name,(repo)=>{
+//               gitcommit(repo[0],(commit)=>{
+//                   console.log(commit)
+//               })
+//             })
+// })
 
+getUser(1)
+.then(user=>githubrepos(user.name))
+.then(repos=>gitcommit(repos[0]))
+.then(commit=>console.log(commit))
+.catch(err=>console.log("Error:",err.message))
 
 console.log("after")
 
-function  getUser(id,callback)
+function  getUser(id)
 {
-    setTimeout(()=>{
-        console.log("User conecting to database")
-        callback({id:id,name:"Moshin ali"})
-    },2000)
+    return new Promise((reslove,reject)=>{
+        setTimeout(()=>{
+            console.log("User conecting to database")
+            reslove({id:id,name:"Moshin ali"})
+        },2000) 
+    })
+   
 
 }
 
-function  githubrepos(id,callback)
+function  githubrepos(user)
 {
-    setTimeout(()=>{
-        console.log("github repo")
-        callback(["repo1","repo2","repo3"])
-    },2000)
+    return new Promise((reslove,reject)=>{
+        setTimeout(()=>{
+            console.log("connectiong github repo")
+            reslove(["repo1","repo2","repo3"])
+        },2000)
+    })
+   
+  
+
+}
+
+function  gitcommit(repo)
+{
+    return new Promise((reslove,reject)=>{
+        setTimeout(()=>{
+            console.log("connectiong github commit..........")
+            reslove(["commit"])
+        },2000)
+    })
+
 
 }
